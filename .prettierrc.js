0bc2d2a234b5a2819c6a03e50f54c37209fda66c
Mohamed Frag <mfrag38@gmail.com>
2022-02-25 15:12:07 +0200

    project initialization

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	semi: true,
+	tabWidth: 4,
+	useTabs: true,
+	jsxSingleQuote: true,
+};
