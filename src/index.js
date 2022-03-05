eabc76d4eb126b11c8698cc2fa76bde0a38d7e0a
Mohamed Frag <mfrag38@gmail.com>
2022-03-06 00:20:16 +0200

    Removed Sign Up Screen From Authentication Logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import RootNavigator from './navigation/RootNavigator';
 import SignInScreen from './screens/Auth/SignInScreen';
-import SignUpScreen from './screens/Auth/SignUpScreen';
 import ContactsScreen from './screens/ContactsScreen';
 import FavoriteContactsScreen from './screens/FavoriteContactsScreen';
 import AdditionalUserInfoScreen from './screens/Auth/AdditionalUserInfoScreen';
